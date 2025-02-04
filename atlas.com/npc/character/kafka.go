df0bbe5ecea22abd6b8e4e7b680af403a4b322b8
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-04 15:57:45 -0500

    Remove accidental check-in

--- atlas.com/npc/character/kafka.go
+++ atlas.com/npc/character/kafka.go
@@ -3,7 +3,6 @@ package character
 const (
 	EnvCommandTopic          = "COMMAND_TOPIC_CHARACTER"
 	CommandRequestChangeMeso = "REQUEST_CHANGE_MESO"
-	CommandRequestChangeFame = "REQUEST_CHANGE_FAME"
 	CommandChangeMap         = "CHANGE_MAP"
 )
 
@@ -22,10 +21,4 @@ type changeMapBody struct {
 
 type requestChangeMesoBody struct {
 	Amount int32 `json:"amount"`
-}
-
-type requestChangeFameBody struct {
-	ActorId   uint32 `json:"actorId"`
-	ActorType string `json:"actorType"`
-	Amount    int8   `json:"amount"`
 }
