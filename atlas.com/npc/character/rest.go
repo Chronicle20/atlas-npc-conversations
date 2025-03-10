3cda09f846ea02f2821461c50cce1d3238887c28
Chronicle20 <a.chronicle.20@gmail.com>
2025-03-10 10:33:11 -0400

    Fix character resource unmarshaling.

--- atlas.com/npc/character/rest.go
+++ atlas.com/npc/character/rest.go
@@ -56,6 +56,41 @@ func (r *RestModel) SetID(idStr string) error {
 	return nil
 }
 
+func (r RestModel) GetReferences() []jsonapi.Reference {
+	return []jsonapi.Reference{
+		{
+			Type: "equipment",
+			Name: "equipment",
+		},
+		{
+			Type: "inventories",
+			Name: "inventories",
+		},
+	}
+}
+
+func (r RestModel) GetReferencedIDs() []jsonapi.ReferenceID {
+	var result []jsonapi.ReferenceID
+	return result
+}
+
+func (r RestModel) GetReferencedStructs() []jsonapi.MarshalIdentifier {
+	var result []jsonapi.MarshalIdentifier
+	return result
+}
+
+func (r *RestModel) SetToOneReferenceID(name, ID string) error {
+	return nil
+}
+
+func (r *RestModel) SetToManyReferenceIDs(name string, IDs []string) error {
+	return nil
+}
+
+func (r *RestModel) SetReferencedStructs(references map[string]map[string]jsonapi.Data) error {
+	return nil
+}
+
 func Extract(rm RestModel) (Model, error) {
 	return Model{
 		id:                 rm.Id,
